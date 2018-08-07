12438d58867a78ba642b4157d8e5b386f7fbb969
Vijayroman <42169869+Vijayroman@users.noreply.github.com>
2018-08-07 14:35:55 +0530

    Update add.java

---
+++ add.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class add
+
+  {
+  public static void main(String args[])
+          {
+           int n;
+           Scanner n=new Scanner(System.in)
+           if(n>0)
+             {
+              System.out.println("The number is positive");
+              }
+            elseif (n==0)
+               {
+                System.out.println("The number is zero");
+                }
+            else
+            {
+               System.out.println("The number is negative");
+               }
+            }
+     }    
